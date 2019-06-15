1651166389e575f1ea0e7c559507cd6990e73066
SaumikSarkar <sarkar.saumik15@gmail.com>
2019-06-16 01:19:46 +0530

    [Project Setup] Server Configured for pointing index.html

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+var rootPath = path.normalize(__dirname);
+
 app.use(express.static("app"));
-app.get('/', function (req, res, next) {
-    res.redirect('/');
+
+app.get('*', function(req, res) {
+    res.sendFile(rootPath + '/app/index.html');
 });
+
 app.listen(8080, 'localhost');
 console.log("AngularJS to Angular Migration Project Server is listening on port 8080");
