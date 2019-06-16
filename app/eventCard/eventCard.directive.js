70a309ba330dcd3ae2554ac557a503497da7390d
SaumikSarkar <sarkar.saumik15@gmail.com>
2019-06-16 20:27:09 +0530

    [Migrating to Components] Component Directive to 1.5 Component

--- app/eventCard/eventCard.directive.js
+++ app/eventCard/eventCard.directive.js
@@ -1,15 +1,9 @@
-demoAppModule.directive('eventCard', function() {
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'eventCard/eventCard.html',
-        scope: {
-            event: '='
-        },
-        controllerAs: '$ctrl',
-        bindToController: true,
-        controller: function() {
-            
-        }
+demoAppModule.component('eventCard', {
+    templateUrl: 'eventCard/eventCard.html',
+    bindings: {
+        event: '='
+    },
+    controller: function () {
+
     }
 });
